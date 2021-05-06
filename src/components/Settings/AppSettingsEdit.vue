3f143287a521cc2f953105dbaa50886e884521c7
fig781 <adeneilers@gmail.com>
2021-05-05 21:26:04 -0700

    update settings edit modal

--- src/components/Settings/AppSettingsEdit.vue
+++ src/components/Settings/AppSettingsEdit.vue
@@ -1,19 +1,23 @@
 <template>
-  <div id="settings-edit-mask">
-    <div id="settings-edit-inner">
-      <h1>Add a new {{ type }}</h1>
+  <div class="settings-edit-mask">
+    <div class="settings-edit-inner">
+      <h1 class="settings-edit-inner__header">Add a new {{ type }}</h1>
       <form @submit.prevent="onSubmit">
         <InputErrorMessage v-if="errorMessage" :message="errorMessage" />
         <input
           type="text"
-          id="input"
+          class="settings-edit-inner__input"
           name="input"
           v-model="value"
           @keyup="remainingCharCount()"
         /><br />
         <p>{{ remainingChars }} characters left</p>
-        <div id="cancel" class="button" @click="$emit('close')">Cancel</div>
-        <input class="button" id="submit" type="submit" value="Submit" />
+        <div class="button-container">
+          <input class="button button--submit" type="submit" value="Submit" />
+          <div class="button button--cancel" @click="$emit('close')">
+            Cancel
+          </div>
+        </div>
       </form>
     </div>
   </div>
@@ -22,6 +26,7 @@
 <script>
   import InputErrorMessage from '../InputErrorMessage';
   import { inputValidation } from '../../globalFunctions/inputValidation';
+
   export default {
     name: 'AppSettingsEdit',
     data() {
@@ -94,7 +99,7 @@
 </script>
 
 <style scoped>
-  #settings-edit-mask {
+  .settings-edit-mask {
     position: fixed;
     z-index: 99;
     left: 0;
@@ -103,37 +108,42 @@
     height: 100%;
     background-color: rgba(0, 0, 0, 0.623);
   }
-  h1 {
-    font-family: 'Montserrat', sans-serif;
-    font-weight: normal;
-  }
-  #settings-edit-inner {
+
+  .settings-edit-inner {
     margin: auto;
     background-color: #fff;
     box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
     border-radius: 3px;
     width: 400px;
-    padding: 40px 40px 60px 40px;
-    margin-top: 5%;
+    padding: 40px 40px 40px 40px;
+    margin-top: 20%;
   }
-  #input {
+
+  .settings-edit-inner__header {
+    font-family: 'Montserrat', sans-serif;
+    font-weight: normal;
+  }
+
+  .settings-edit-inner__input {
     width: 100%;
     padding: 5px 10px;
     margin: 8px 0;
     box-sizing: border-box;
     font-size: 18px;
-  }
-  input {
     border: solid 1px black;
   }
-  input:focus {
+
+  .settings-edit-inner__input:focus {
     border: solid 1px #0069ff;
     outline: #0069ff;
   }
+
+  .button-container {
+    margin-top: 8px;
+    display: flex;
+    justify-content: flex-end;
+  }
   .button {
-    float: right;
-    margin-top: 5px;
-    margin-bottom: 13px;
     width: 8rem;
     padding: 10px;
     border: none;
@@ -143,29 +153,34 @@
     cursor: pointer;
     text-align: center;
   }
-  #submit {
+
+  .button--submit {
     background-color: #1f43b8;
     color: white;
     margin-right: 5px;
   }
-  #submit:hover {
+
+  .button--submit:hover {
     background: #2a54b1;
   }
-  #cancel {
+
+  .button--cancel {
     background-color: #ced6e7;
     color: black;
   }
-  #cancel:hover {
+
+  .button--cancel:hover {
     background-color: #afb8cc;
   }
+
   @media only screen and (max-width: 768px) {
-    #settings-edit-inner {
+    .settings-edit-inner {
       width: 250px;
       padding: 20px 20px 40px 20px;
     }
-    .button {
-      padding: 4px;
-      width: 5rem;
+
+    .button-container {
+      justify-content: center;
     }
   }
 </style>
